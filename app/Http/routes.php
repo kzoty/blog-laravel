f070153f20f55af782db5014ffb060f28caf9d3a
Luis Casotti <casotti@gmail.com>
2015-11-02 16:39:36 -0200

    Fixing routes in groups

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,9 +19,15 @@ Route::get('/', 'PostsController@index');
 Route::get('admin', function() {
 	return redirect()->route('admin.post.list');
 });
-Route::get('admin/posts', ['as'=>'admin.post.list','uses'=>'PostsAdminController@index']);
-Route::get('admin/posts/create-post', ['as'=>'admin.post.create','uses'=>'PostsAdminController@create']);
-Route::post('admin/posts/store-post', ['as'=>'admin.post.store','uses'=>'PostsAdminController@store']);
-Route::get('admin/posts/edit/{id}', ['as'=>'admin.post.edit','uses'=>'PostsAdminController@edit']);
-Route::put('admin/posts/update/{id}', ['as'=>'admin.post.update','uses'=>'PostsAdminController@update']);
-Route::get('admin/posts/destroy/{id}', ['as'=>'admin.post.destroy','uses'=>'PostsAdminController@destroy']);
+
+Route::group(['prefix'=>'admin'], function(){
+	Route::group(['prefix'=>'posts'], function(){
+		Route::get('index', ['as'=>'admin.post.list','uses'=>'PostsAdminController@index']);
+		Route::get('create-post', ['as'=>'admin.post.create','uses'=>'PostsAdminController@create']);
+		Route::post('store-post', ['as'=>'admin.post.store','uses'=>'PostsAdminController@store']);
+		Route::get('edit/{id}', ['as'=>'admin.post.edit','uses'=>'PostsAdminController@edit']);
+		Route::put('update/{id}', ['as'=>'admin.post.update','uses'=>'PostsAdminController@update']);
+		Route::get('destroy/{id}', ['as'=>'admin.post.destroy','uses'=>'PostsAdminController@destroy']);
+	});
+});
+
